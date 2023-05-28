e81cdb72eb332dc0fd34ad5382d5f011bc6ec3e1
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-05-28 19:07:16 +0200

    update: added routing for app(unfinished)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import { Button } from '@nextui-org/react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Genre from './pages/Genre';
+import MovieDetails from './pages/MovieDetails';
+import TopRated from './pages/TopRated';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import ROUTES from './routes/routes';
 
 function App() {
    return (
-      <main>
-         <Button>click me</Button>
-      </main>
+      <div className="App">
+         <Header />
+         <main>
+            <BrowserRouter>
+               <Routes>
+                  <Route
+                     path="/"
+                     element={<Navigate replace to={`/${ROUTES.HOME}`} />}
+                  ></Route>
+                  <Route path={ROUTES.HOME} element={<Home />}></Route>
+                  <Route path={ROUTES.GENRE} element={<Genre />}></Route>
+                  <Route path={ROUTES.TOP_RATED} element={<TopRated />}></Route>
+                  <Route
+                     path={ROUTES.MOVIE_DETAILS}
+                     element={<MovieDetails />}
+                  ></Route>
+               </Routes>
+            </BrowserRouter>
+         </main>
+         <Footer />
+      </div>
    );
 }
 
