49f01733f9fe832a3130ae319c62926d37184f09
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-04 19:44:45 +0200

    update: renamed route for movies genre, added new route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,9 @@
 const ROUTES = {
    HOME: 'moviedb',
-   GENRE: 'moviedb/genre',
+   GENRE: 'moviedb/movie/genre',
    TOP_RATED: 'moviedb/top-rated',
    MOVIE_DETAILS: 'moviedb/movie-details',
+   SERIES_DETAILS: 'moviedb/series-details',
 };
 
 export default ROUTES;
