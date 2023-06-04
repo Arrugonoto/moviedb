20168a36a74df33baf4c5b3c5928c1241053d2f7
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-04 19:42:03 +0200

    update: updated styling for navbar elements, fixed responsiveness issues

--- src/components/navbar/Nav.tsx
+++ src/components/navbar/Nav.tsx
@@ -9,35 +9,48 @@ import {
 import styles from './nav.module.css';
 import { NavLink } from 'react-router-dom';
 import { GENRES } from '../../data/genres';
+import { IoSearchOutline, IoMoon } from 'react-icons/io5';
+import { IoMdSunny } from 'react-icons/io';
 
 const Nav = () => {
    const { isDark } = useTheme();
 
    const handleChange = () => {
       const nextTheme = isDark ? 'light' : 'dark';
-      window.localStorage.setItem('data-theme', nextTheme); // you can use any storage
+      window.localStorage.setItem('data-theme', nextTheme);
       changeTheme(nextTheme);
    };
 
    return (
-      <Navbar maxWidth="fluid" css={{ py: '0.3rem' }}>
+      <Navbar maxWidth="fluid" css={{ py: '.4rem' }} height="auto">
          <Container fluid css={{ fd: 'column', p: '0 .2rem' }}>
             <Navbar.Content css={{ jc: 'flex-end', ai: 'center' }}>
-               <Input bordered size="sm" />
-               <Switch checked={isDark} onChange={handleChange} />
+               <Input
+                  type="search"
+                  size="xs"
+                  bordered
+                  clearable
+                  aria-label="Input search"
+                  placeholder="Search by title/series"
+                  contentRight={<IoSearchOutline />}
+                  color="primary"
+               />
+               <Switch
+                  checked={isDark}
+                  onChange={handleChange}
+                  size="sm"
+                  iconOn={<IoMoon />}
+                  iconOff={<IoMdSunny />}
+               />
             </Navbar.Content>
-            <Navbar.Content
-               css={{ ox: 'auto', p: '.8rem 0rem' }}
-               className={styles.container_scrollbar}
-            >
+            <Navbar.Content className={styles.container_scrollbar}>
                {GENRES.map(
                   (genre: { id: number; name: string }): JSX.Element => (
                      <NavLink
                         key={genre.id}
-                        to={`/moviedb/genre/${genre.id}`}
-                        style={{ whiteSpace: 'nowrap' }}
+                        to={`/moviedb/movie/genre/${genre.id}`}
                         className={({ isActive, isPending }) =>
-                           isPending ? 'pending' : isActive ? 'active' : ''
+                           isPending ? 'pending' : isActive ? styles.active : ''
                         }
                      >
                         {genre.name}
