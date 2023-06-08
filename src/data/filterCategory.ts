02ea6a48bc26f800bf357c3bd3b86af041fa5940
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-08 22:12:14 +0200

    rearrange: added static file with key values for filtering fetched data

---
+++ src/data/filterCategory.ts
@@ -0,0 +1,12 @@
+export const FILTER_BY: { name: string; value: string }[] = [
+   { name: 'Popularity - asc', value: 'popularity.asc' },
+   { name: 'Popularity - desc', value: 'popularity.desc' },
+   { name: 'Revenue - asc', value: 'revenue.asc' },
+   { name: 'Revenue - desc', value: 'revenue.desc' },
+   { name: 'Release date - asc', value: 'primary_release_date.asc' },
+   { name: 'Release date - desc', value: 'primary_release_date.desc' },
+   { name: 'Rating - asc', value: 'vote_average.asc' },
+   { name: 'Rating - desc', value: 'vote_average.desc' },
+   { name: 'Vote count - asc', value: 'vote_count.asc' },
+   { name: 'Vote count - desc ', value: 'vote_count.desc' },
+];
