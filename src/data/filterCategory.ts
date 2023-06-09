9f0d6075d6cabfa68b325679cfa941d89ad357e7
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-09 12:42:21 +0200

    update: changed order of values

--- src/data/filterCategory.ts
+++ src/data/filterCategory.ts
@@ -1,12 +1,12 @@
 export const FILTER_BY: { name: string; value: string }[] = [
-   { name: 'Popularity - asc', value: 'popularity.asc' },
    { name: 'Popularity - desc', value: 'popularity.desc' },
-   { name: 'Revenue - asc', value: 'revenue.asc' },
+   { name: 'Popularity - asc', value: 'popularity.asc' },
    { name: 'Revenue - desc', value: 'revenue.desc' },
-   { name: 'Release date - asc', value: 'primary_release_date.asc' },
+   { name: 'Revenue - asc', value: 'revenue.asc' },
    { name: 'Release date - desc', value: 'primary_release_date.desc' },
-   { name: 'Rating - asc', value: 'vote_average.asc' },
+   { name: 'Release date - asc', value: 'primary_release_date.asc' },
    { name: 'Rating - desc', value: 'vote_average.desc' },
-   { name: 'Vote count - asc', value: 'vote_count.asc' },
-   { name: 'Vote count - desc ', value: 'vote_count.desc' },
+   { name: 'Rating - asc', value: 'vote_average.asc' },
+   { name: 'Vote count - desc', value: 'vote_count.desc' },
+   { name: 'Vote count - asc ', value: 'vote_count.asc' },
 ];
