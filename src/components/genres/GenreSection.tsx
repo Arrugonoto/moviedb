d24426500e4f4bbb0d8dde0eaf12bf3806527744
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-13 19:47:55 +0200

    feat: added infinite scrolling functionality for genre sections

--- src/components/genres/GenreSection.tsx
+++ src/components/genres/GenreSection.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback, ReactElement } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Grid, Container, Button } from '@nextui-org/react';
 import { useParams } from 'react-router-dom';
 import { API_KEY } from '../../services/api-key';
@@ -6,6 +6,7 @@ import { METHODS, API_ENDPOINT } from '../../services/api';
 import MovieCard from '../moviecard/MovieCard';
 import SelectFilter from './SelectFilter';
 import useFetchMovies from '../../hooks/useFetchMovies';
+import { useIntersection } from '@mantine/hooks';
 
 type OptionsTypes = {
    method: string;
@@ -19,9 +20,13 @@ const GenreSection = () => {
    const { genreId } = useParams();
    const [page, setPage] = useState<number>(1);
    const [sortType, setSortType] = useState<string>('popularity.desc');
-   const observer = useRef<HTMLDivElement | null>(null);
-   const { handleFetch, movies, setMovies, loading, lastPage } =
-      useFetchMovies();
+   const { handleFetch, movies, setMovies, lastPage } = useFetchMovies();
+   const lastMovieRef = useRef<HTMLElement>(null);
+
+   const { ref, entry } = useIntersection({
+      root: lastMovieRef.current,
+      threshold: 1,
+   });
 
    // const lastMovieRef = useCallback(
    //    node => {
@@ -61,7 +66,13 @@ const GenreSection = () => {
       if (page !== 1) {
          fetchMovies();
       }
+      // eslint-disable-next-line
    }, [page]);
+
+   useEffect(() => {
+      if (entry?.isIntersecting && page < lastPage) setPage(prev => prev + 1);
+      // eslint-disable-next-line
+   }, [entry]);
 
    return (
       <Container direction="column" fluid={true}>
@@ -69,22 +80,41 @@ const GenreSection = () => {
             <SelectFilter setSortType={setSortType} />
          </Container>
          <Grid.Container gap={2} justify="center">
-            {movies?.map(movie => (
-               <Grid key={movie.id}>
-                  <MovieCard
-                     id={movie.id}
-                     backdrop_path={movie.backdrop_path}
-                     original_title={movie.original_title}
-                     overview={movie.overview}
-                     popularity={movie.popularity}
-                     poster_path={movie.poster_path}
-                     release_date={movie.release_date}
-                     title={movie.title}
-                     vote_average={movie.vote_average}
-                     vote_count={movie.vote_count}
-                  ></MovieCard>
-               </Grid>
-            ))}
+            {movies?.map((movie, i) => {
+               if (i === movies.length - 1)
+                  return (
+                     <Grid key={movie.id} ref={ref}>
+                        <MovieCard
+                           id={movie.id}
+                           backdrop_path={movie.backdrop_path}
+                           original_title={movie.original_title}
+                           overview={movie.overview}
+                           popularity={movie.popularity}
+                           poster_path={movie.poster_path}
+                           release_date={movie.release_date}
+                           title={movie.title}
+                           vote_average={movie.vote_average}
+                           vote_count={movie.vote_count}
+                        ></MovieCard>
+                     </Grid>
+                  );
+               return (
+                  <Grid key={movie.id}>
+                     <MovieCard
+                        id={movie.id}
+                        backdrop_path={movie.backdrop_path}
+                        original_title={movie.original_title}
+                        overview={movie.overview}
+                        popularity={movie.popularity}
+                        poster_path={movie.poster_path}
+                        release_date={movie.release_date}
+                        title={movie.title}
+                        vote_average={movie.vote_average}
+                        vote_count={movie.vote_count}
+                     ></MovieCard>
+                  </Grid>
+               );
+            })}
             <Button onPress={() => setPage(prev => prev + 1)}>
                Fetch more
             </Button>
