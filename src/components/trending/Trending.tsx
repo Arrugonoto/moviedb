de4ba51b5140386539a5a1e557e933891f191419
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-21 19:33:32 +0200

    update: updated styling of component

--- src/components/trending/Trending.tsx
+++ src/components/trending/Trending.tsx
@@ -3,25 +3,53 @@ import { Container, Row, Text, Button } from '@nextui-org/react';
 import TrendingMovies from './TrendingMovies';
 import TrendingSeries from './TrendingSeries';
 import TrendingPeople from './TrendingPeople';
+import style from './trending.module.css';
 
 const Trending = () => {
-   const [displayTrending, setDisplayTrending] = useState('movies');
+   const [displayTrending, setDisplayTrending] = useState<string>('movies');
 
    return (
       <section>
          <Container fluid>
             <Row>
-               <Text>Trending</Text>
+               <Text
+                  h1
+                  size={26}
+                  css={{
+                     textGradient: '45deg, $primary 20%, $accent600 100%',
+                     letterSpacing: '0.5px',
+                  }}
+               >
+                  Trending
+               </Text>
             </Row>
-            <Row>
-               <Button.Group>
-                  <Button onClick={() => setDisplayTrending('movies')}>
+            <Row justify="center">
+               <Button.Group light rounded size="lg">
+                  <Button
+                     onPress={() => setDisplayTrending('movies')}
+                     css={{
+                        color: displayTrending === 'movies' ? '#9210a0' : '',
+                     }}
+                     className={style.trendingBtn}
+                  >
                      Movies
                   </Button>
-                  <Button onClick={() => setDisplayTrending('series')}>
+                  <Button
+                     onPress={() => setDisplayTrending('series')}
+                     css={{
+                        color: displayTrending === 'series' ? '#9210a0' : '',
+                     }}
+                     className={style.trendingBtn}
+                  >
                      TV Series
                   </Button>
-                  <Button onClick={() => setDisplayTrending('people')}>
+                  <Button
+                     onPress={() => setDisplayTrending('people')}
+                     css={{
+                        color: displayTrending === 'people' ? '#9210a0' : '',
+                     }}
+                     className={style.trendingBtn}
+                  >
                      People
                   </Button>
                </Button.Group>
