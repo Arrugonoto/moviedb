77cf45ee3b9940b2e0aef3e1de8ffa5d604bf2bd
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-21 19:37:42 +0200

    rearrange: added component with different styling for card(unfinished)

---
+++ src/components/moviecard/CardMdBlur.tsx
@@ -0,0 +1,122 @@
+import { ReactElement } from 'react';
+import { Card, Text, Col, Row, Tooltip, useTheme } from '@nextui-org/react';
+import { FaImage, FaStar, FaUser } from 'react-icons/fa';
+import style from './movieCard.module.css';
+
+interface PropTypes {
+   backdrop_path: string;
+   id: number;
+   original_title: string;
+   overview: string;
+   popularity: number;
+   poster_path: string;
+   release_date: string;
+   title: string;
+   vote_average: number;
+   vote_count: number;
+}
+
+const CardMdBlur = ({
+   backdrop_path,
+   id,
+   original_title,
+   overview,
+   popularity,
+   poster_path,
+   release_date,
+   title,
+   vote_average,
+   vote_count,
+}: PropTypes): ReactElement => {
+   const { isDark } = useTheme();
+
+   return (
+      <div>
+         <Card
+            isPressable
+            allowTextSelectionOnPress={true}
+            css={{ w: '12.3rem', h: '22.2rem', br: '.5rem' }}
+            className={style.card}
+         >
+            <Card.Header
+               css={{
+                  position: 'absolute',
+                  h: '1.6rem',
+                  bf: 'blur(5px)',
+                  bc: `${
+                     isDark ? 'rgba(8, 8, 8, .8)' : 'rgba(250, 250, 250, .8)'
+                  }`,
+               }}
+               className={style.cardHeader}
+            >
+               <Row align="center" justify="center" css={{ gap: '.4rem;' }}>
+                  <Tooltip content={'Rating'} shadow={true}>
+                     <FaStar style={{ fontSize: '1.2rem', color: '#9210A0' }} />
+                  </Tooltip>
+                  <Text>{vote_average}</Text>
+               </Row>
+               <Row align="center" justify="center" css={{ gap: '.4rem;' }}>
+                  <Tooltip content={'Vote count'} shadow={true}>
+                     <FaUser style={{ fontSize: '1.2rem', color: '#9210A0' }} />
+                  </Tooltip>
+                  <Text>{vote_count}</Text>
+               </Row>
+            </Card.Header>
+            <Card.Body css={{ p: '0' }}>
+               {poster_path ? (
+                  <Col css={{ h: '18rem', ov: 'hidden' }}>
+                     <Card.Image
+                        src={`https://image.tmdb.org/t/p/w400${poster_path}`}
+                        width="100%"
+                        height="100%"
+                        objectFit="cover"
+                        alt="Movie poster"
+                        loading="lazy"
+                        css={{ br: '0 0 .6rem .6rem' }}
+                        className={style.cardImage}
+                     />
+                  </Col>
+               ) : (
+                  <Col
+                     css={{
+                        d: 'flex',
+                        fd: ' column',
+                        h: '18rem',
+                        w: '100%',
+                        jc: 'center',
+                        ai: 'center',
+                        bc: 'rgba(176, 240, 251, .2)',
+                        gap: '.5rem',
+                     }}
+                  >
+                     <Text>No Image Available</Text>
+                     <FaImage style={{ fontSize: '2rem' }} />
+                  </Col>
+               )}
+
+               <Card.Footer
+                  css={{
+                     position: 'absolute',
+                     p: '.5rem .5rem 0',
+                  }}
+               >
+                  <Text
+                     className={style.cardTitle}
+                     size={15}
+                     css={{
+                        ta: 'center',
+                        fontWeight: '600',
+                        letterSpacing: '0.05px',
+                     }}
+                     title={title}
+                  >
+                     {title}
+                  </Text>
+               </Card.Footer>
+            </Card.Body>
+         </Card>
+      </div>
+   );
+};
+
+export default CardMdBlur;
