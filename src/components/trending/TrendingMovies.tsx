754ce5061b4f1e053c3da2d470fc8041003b732c
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-21 19:34:56 +0200

    update: added structure and styling for component and fetched elements(unfinished)

--- src/components/trending/TrendingMovies.tsx
+++ src/components/trending/TrendingMovies.tsx
@@ -1,5 +1,63 @@
+import { useEffect } from 'react';
+import { METHODS } from '../../services/api';
+import { API_KEY } from '../../services/api-key';
+import useFetch from '../../hooks/useFetch';
+import { Container } from '@nextui-org/react';
+import CardMdBlur from '../moviecard/CardMdBlur';
+
+interface OptionsTypes {
+   method: string;
+   headers: {
+      accept: string;
+      Authorization: string;
+   };
+}
+
 const TrendingMovies = () => {
-   return <div>TrendingMovies</div>;
+   const { handleFetch, data } = useFetch();
+
+   const fetchMovies = async (): Promise<void> => {
+      const options: OptionsTypes = {
+         method: METHODS.GET,
+         headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY.access_token}`,
+         },
+      };
+      const url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US`;
+
+      handleFetch({ url, options });
+   };
+
+   useEffect(() => {
+      fetchMovies();
+   }, []);
+
+   return (
+      <Container
+         display="flex"
+         direction="row"
+         wrap="nowrap"
+         gap={0}
+         css={{ ox: 'scroll', py: '1rem', gap: '1rem' }}
+      >
+         {data.map(movie => (
+            <CardMdBlur
+               key={movie.id}
+               id={movie.id}
+               backdrop_path={movie.backdrop_path}
+               original_title={movie.original_title}
+               overview={movie.overview}
+               popularity={movie.popularity}
+               poster_path={movie.poster_path}
+               release_date={movie.release_date}
+               title={movie.title}
+               vote_average={movie.vote_average}
+               vote_count={movie.vote_count}
+            />
+         ))}
+      </Container>
+   );
 };
 
 export default TrendingMovies;
