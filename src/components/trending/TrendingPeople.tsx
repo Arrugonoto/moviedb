9c2dcab6fe68292cd1fe387d7a6ebbdf2b5971d8
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-25 19:17:15 +0200

    update: finished building components for displaying trending TV series and people

--- src/components/trending/TrendingPeople.tsx
+++ src/components/trending/TrendingPeople.tsx
@@ -1,5 +1,83 @@
+import { useEffect, useRef } from 'react';
+import { METHODS } from '../../services/api';
+import { API_KEY } from '../../services/api-key';
+import useFetch from '../../hooks/useFetch';
+import CardMdBlurPeople from '../moviecard/CardMdBlurPeople';
+import { motion } from 'framer-motion';
+
+interface OptionsTypes {
+   method: string;
+   headers: {
+      accept: string;
+      Authorization: string;
+   };
+}
+
+interface PeopleProps {
+   adult: boolean;
+   id: number;
+   name: string;
+   original_name: string;
+   media_type: string;
+   popularity: number;
+   gender: 1;
+   known_for_department: string;
+   profile_path: string;
+   known_for: object[];
+}
+
 const TrendingPeople = () => {
-   return <div>TrendingPeople</div>;
+   const { handleFetch, data } = useFetch<PeopleProps[]>([]);
+   const constraintsRef = useRef<HTMLDivElement>(null);
+
+   const fetchSeries = async (): Promise<void> => {
+      const options: OptionsTypes = {
+         method: METHODS.GET,
+         headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY.access_token}`,
+         },
+      };
+      const url = `https://api.themoviedb.org/3/trending/person/day?language=en-US`;
+
+      handleFetch({ url, options });
+   };
+
+   useEffect(() => {
+      fetchSeries();
+      console.log(data);
+      // eslint-disable-next-line
+   }, []);
+
+   return (
+      <motion.div
+         ref={constraintsRef}
+         style={{
+            display: 'flex',
+            overflowX: 'hidden',
+            gap: '1.2rem',
+            padding: '.5rem 1rem',
+         }}
+      >
+         <motion.div
+            drag="x"
+            dragConstraints={constraintsRef}
+            style={{ display: 'flex', gap: '1.2rem' }}
+            dragElastic={0.1}
+         >
+            {data?.map(person => (
+               <CardMdBlurPeople
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  popularity={person.popularity}
+                  original_name={person.original_name}
+                  profile_path={person.profile_path}
+               />
+            ))}
+         </motion.div>
+      </motion.div>
+   );
 };
 
 export default TrendingPeople;
--- src/components/trending/TrendingSeries.tsx
+++ src/components/trending/TrendingSeries.tsx
@@ -1,5 +1,90 @@
+import { useEffect, useRef } from 'react';
+import { METHODS } from '../../services/api';
+import { API_KEY } from '../../services/api-key';
+import useFetch from '../../hooks/useFetch';
+import CardMdBlur from '../moviecard/CardMdBlur';
+import { motion } from 'framer-motion';
+
+interface OptionsTypes {
+   method: string;
+   headers: {
+      accept: string;
+      Authorization: string;
+   };
+}
+
+interface SeriesProps {
+   adult: boolean;
+   backdrop_path: string;
+   genre_ids: number[];
+   id: number;
+   original_language: string;
+   original_name: string;
+   overview: string;
+   popularity: number;
+   poster_path: string;
+   first_air_date: string;
+   name: string;
+   vote_average: number;
+   vote_count: number;
+   media_type: string;
+   origin_country: string[];
+}
+
 const TrendingSeries = () => {
-   return <div>TrendingSeries</div>;
+   const { handleFetch, data } = useFetch<SeriesProps[]>([]);
+   const constraintsRef = useRef<HTMLDivElement>(null);
+
+   const fetchSeries = async (): Promise<void> => {
+      const options: OptionsTypes = {
+         method: METHODS.GET,
+         headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY.access_token}`,
+         },
+      };
+      const url = `https://api.themoviedb.org/3/trending/tv/day?language=en-US`;
+
+      handleFetch({ url, options });
+   };
+
+   useEffect(() => {
+      fetchSeries();
+      // eslint-disable-next-line
+   }, []);
+
+   return (
+      <motion.div
+         ref={constraintsRef}
+         style={{
+            display: 'flex',
+            overflowX: 'hidden',
+            gap: '1.2rem',
+            padding: '.5rem 1rem',
+         }}
+      >
+         <motion.div
+            drag="x"
+            dragConstraints={constraintsRef}
+            style={{ display: 'flex', gap: '1.2rem' }}
+            dragElastic={0.1}
+         >
+            {data?.map(series => (
+               <CardMdBlur
+                  key={series.id}
+                  id={series.id}
+                  overview={series.overview}
+                  popularity={series.popularity}
+                  poster_path={series.poster_path}
+                  first_air_date={series.first_air_date}
+                  name={series.name}
+                  vote_average={series.vote_average}
+                  vote_count={series.vote_count}
+               />
+            ))}
+         </motion.div>
+      </motion.div>
+   );
 };
 
 export default TrendingSeries;
