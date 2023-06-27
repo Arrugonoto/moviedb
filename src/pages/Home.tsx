ad65d05b502db45cfc4e44183d049b00a6e51c39
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-27 19:50:59 +0200

    update: updated styling, added components for displaying popular and top rated movies/tv shows

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import { Container } from '@nextui-org/react';
 import Trending from '../components/trending/Trending';
+import Popular from '../components/popular/Popular';
+import TopRated from '../components/toprated/TopRated';
 
 const Home = () => {
    return (
-      <Container fluid>
+      <Container fluid css={{ d: 'flex', fd: 'column', gap: '3rem' }}>
          <Trending />
+         <Popular />
+         <TopRated />
       </Container>
    );
 };
