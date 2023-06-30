c9d823907d1d4da0d79eb1a21de72999ce88d201
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-06-30 20:45:47 +0200

    update: removed placeholder element, used for testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,19 @@ import { Container } from '@nextui-org/react';
 import Trending from '../components/trending/Trending';
 import Popular from '../components/popular/Popular';
 import TopRated from '../components/toprated/TopRated';
+import CardLgAnimated from '../components/card/CardLgAnimated';
 
 const Home = () => {
    return (
-      <Container fluid css={{ d: 'flex', fd: 'column', gap: '3rem' }}>
+      <Container
+         fluid
+         css={{
+            d: 'flex',
+            fd: 'column',
+            gap: '3rem',
+            p: '.5rem',
+         }}
+      >
          <Trending />
          <Popular />
          <TopRated />
