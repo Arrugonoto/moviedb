4b9eaa5ff43fa2691398b38987451b8b288a8a85
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-07-02 20:38:39 +0200

    update: added array of objects which contains data about tv series ids and names based on themoviedb api data

--- src/data/genres.ts
+++ src/data/genres.ts
@@ -77,3 +77,70 @@ export const GENRES: { id: number; name: string }[] = [
       name: 'Western',
    },
 ];
+
+export const SERIES_GENRES: { id: number; name: string }[] = [
+   {
+      id: 10759,
+      name: 'Action & Adventure',
+   },
+   {
+      id: 16,
+      name: 'Animation',
+   },
+   {
+      id: 35,
+      name: 'Comedy',
+   },
+   {
+      id: 80,
+      name: 'Crime',
+   },
+   {
+      id: 99,
+      name: 'Documentary',
+   },
+   {
+      id: 18,
+      name: 'Drama',
+   },
+   {
+      id: 10751,
+      name: 'Family',
+   },
+   {
+      id: 10762,
+      name: 'Kids',
+   },
+   {
+      id: 9648,
+      name: 'Mystery',
+   },
+   {
+      id: 10763,
+      name: 'News',
+   },
+   {
+      id: 10764,
+      name: 'Reality',
+   },
+   {
+      id: 10765,
+      name: 'Sci-Fi & Fantasy',
+   },
+   {
+      id: 10766,
+      name: 'Soap',
+   },
+   {
+      id: 10767,
+      name: 'Talk show',
+   },
+   {
+      id: 10768,
+      name: 'War & Politics',
+   },
+   {
+      id: 37,
+      name: 'Western',
+   },
+];
