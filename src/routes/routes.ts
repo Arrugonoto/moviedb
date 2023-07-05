bdd8c8d74405884460eadce1f1049608b0fd5403
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-07-05 19:28:12 +0200

    update(routes): added new routes and renamed route for top rated movies

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,8 @@
 const ROUTES = {
    HOME: 'moviedb',
    GENRE: 'moviedb/movie/genre',
-   TOP_RATED: 'moviedb/top-rated',
+   TOP_RATED_MOVIES: 'moviedb/top-rated/movie',
+   TOP_RATED_SERIES: 'moviedb/top-rated/tv-series',
    MOVIE_DETAILS: 'moviedb/movie-details',
    SERIES_DETAILS: 'moviedb/series-details',
 };
