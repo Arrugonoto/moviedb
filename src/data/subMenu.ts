346cc2f3ec1693aa972218ab1f1929bd22267283
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-07-07 19:12:15 +0200

    rearrange(subMenu): added static data with names and routes rendered in sub menu

---
+++ src/data/subMenu.ts
@@ -0,0 +1,12 @@
+import ROUTES from '../routes/routes';
+
+export const MOVIES = [
+   { name: 'Popular', route: ROUTES.MOVIES_POPULAR },
+   { name: 'Upcoming', route: ROUTES.MOVIES_UPCOMING },
+   { name: 'Top Rated', route: ROUTES.MOVIES_TOP_RATED },
+];
+
+export const SERIES = [
+   { name: 'Popular', route: ROUTES.SERIES_POPULAR },
+   { name: 'Top Rated', route: ROUTES.SERIES_TOP_RATED },
+];
