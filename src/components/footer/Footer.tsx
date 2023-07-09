fbbe2764f40cc2b58d1bf48850eaaea9ce618a93
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-07-09 18:07:46 +0200

    update(Footer): updated styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,25 @@
+import { Container } from '@nextui-org/react';
+
 const Footer = () => {
-   return <footer>I am a footer</footer>;
+   return (
+      <footer style={{ width: '100%', marginTop: '3rem' }}>
+         <section style={{ width: '100%' }}>
+            <Container
+               fluid
+               css={{
+                  d: 'flex',
+                  jc: 'center',
+                  bc: '$violet500',
+                  p: '3rem 0',
+                  color: '#fafafa',
+                  maxWidth: '100%',
+               }}
+            >
+               I am a footer
+            </Container>
+         </section>
+      </footer>
+   );
 };
 
 export default Footer;
