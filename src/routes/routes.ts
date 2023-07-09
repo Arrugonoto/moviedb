13ce17708b7b1a1d42f9243b58b2e3f8fb808e2b
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-07-09 18:04:49 +0200

    update: added routes to pages(finished)

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
-const ROUTES = {
+const ROUTES: { [key: string]: string } = {
    HOME: 'moviedb',
+   MOVIES: 'moviedb/movies',
    MOVIE_GENRE: 'moviedb/movie/genre',
    MOVIES_POPULAR: 'moviedb/popular/movie',
    MOVIES_TOP_RATED: 'moviedb/top-rated/movie',
    MOVIES_UPCOMING: 'moviedb/upcoming/movies',
    MOVIE_DETAILS: 'moviedb/movie/details', // /movie-title/id
+   SERIES: 'moviedb/series',
    SERIES_GENRE: 'moviedb/series/genre',
    SERIES_POPULAR: 'moviedb/popular/series',
    SERIES_TOP_RATED: 'moviedb/top-rated/tv-series',
