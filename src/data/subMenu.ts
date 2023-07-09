639b8b0f7f589b2e7a991fe9462602e07c836529
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-07-09 18:05:32 +0200

    update: added missing types

--- src/data/subMenu.ts
+++ src/data/subMenu.ts
@@ -1,12 +1,12 @@
 import ROUTES from '../routes/routes';
 
-export const MOVIES = [
+export const MOVIES: { name: string; route: string }[] = [
    { name: 'Popular', route: ROUTES.MOVIES_POPULAR },
    { name: 'Upcoming', route: ROUTES.MOVIES_UPCOMING },
    { name: 'Top Rated', route: ROUTES.MOVIES_TOP_RATED },
 ];
 
-export const SERIES = [
+export const SERIES: { name: string; route: string }[] = [
    { name: 'Popular', route: ROUTES.SERIES_POPULAR },
    { name: 'Top Rated', route: ROUTES.SERIES_TOP_RATED },
 ];
