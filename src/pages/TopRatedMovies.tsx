abdbdb46679ff460ea591eb49d69a894f0194a04
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-07-10 19:48:32 +0200

    update(TopRatedMovies-page): updated structure and styling of component

--- src/pages/TopRatedMovies.tsx
+++ src/pages/TopRatedMovies.tsx
@@ -1,5 +1,93 @@
+import { useEffect } from 'react';
+import { Container, Row, Text } from '@nextui-org/react';
+import { METHODS } from '../services/api';
+import { API_KEY } from '../services/api-key';
+import useFetch from '../hooks/useFetch';
+import CardList from '../components/card/CardList';
+
+interface OptionsTypes {
+   method: string;
+   headers: {
+      accept: string;
+      Authorization: string;
+   };
+}
+
+interface MovieProps {
+   adult: boolean;
+   backdrop_path: string;
+   genre_ids: number[];
+   id: number;
+   original_language: string;
+   original_title: string;
+   overview: string;
+   popularity: number;
+   poster_path: string;
+   release_date: string;
+   title: string;
+   video: boolean;
+   vote_average: number;
+   vote_count: number;
+   origin_country: string[];
+}
+
 const TopRated = () => {
-   return <section style={{ minHeight: '100dvh' }}>top rated</section>;
+   const { handleFetch, data } = useFetch<MovieProps[]>([]);
+
+   const fetchMovies = async (): Promise<void> => {
+      const options: OptionsTypes = {
+         method: METHODS.GET,
+         headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY.access_token}`,
+         },
+      };
+      const url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`;
+
+      handleFetch({ url, options });
+   };
+
+   useEffect(() => {
+      fetchMovies();
+      // eslint-disable-next-line
+   }, []);
+
+   return (
+      <section style={{ minHeight: '100dvh' }}>
+         <Container fluid>
+            <Row>
+               <Text
+                  h1
+                  size={26}
+                  css={{
+                     textGradient: '45deg, $accent400 10%, $violet400 100%',
+                     letterSpacing: '0.5px',
+                  }}
+               >
+                  Top 100 Movies
+               </Text>
+            </Row>
+            <Row css={{ d: 'flex', fd: 'column', gap: '1rem' }}>
+               {data.map(movie => (
+                  <CardList
+                     key={movie.id}
+                     id={movie.id}
+                     overview={movie.overview}
+                     popularity={movie.popularity}
+                     poster_path={movie.poster_path}
+                     release_date={movie.release_date}
+                     title={movie.title}
+                     vote_average={movie.vote_average}
+                     vote_count={movie.vote_count}
+                     genre_ids={movie.genre_ids}
+                     origin_country={movie.origin_country}
+                     video={movie.video}
+                  />
+               ))}
+            </Row>
+         </Container>
+      </section>
+   );
 };
 
 export default TopRated;
