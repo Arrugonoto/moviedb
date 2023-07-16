fcd356d34a073b8ff3fa9ebd75a5c2644b0bd525
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-07-16 19:16:55 +0200

    rearrange(useInfiniteFetch): added hook for handling infinite query with generic types

---
+++ src/hooks/useInfiniteFetch.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+interface FetchTypes {
+   url: Parameters<typeof fetch>[0];
+   options?: Parameters<typeof fetch>[1];
+}
+
+const useInfiniteFetch = <T,>(initialData: T) => {
+   const [loading, setLoading] = useState<boolean>(false);
+   const [error, setError] = useState<unknown>(null);
+   const [data, setData] = useState<T>(initialData);
+   const [lastPage, setLastPage] = useState<number>(0);
+
+   const handleFetch = async ({ url, options }: FetchTypes): Promise<void> => {
+      setLoading(true);
+
+      const response = await fetch(url, options);
+      const result = await response.json();
+
+      if (!response.ok) {
+         console.error(result);
+         setError(result);
+         throw new Error(`Couldn't fetch source`);
+      }
+      setData(
+         (prev: T) =>
+            [
+               ...new Map(
+                  [...(prev as T[]), ...result.results].map(m => [m.id, m])
+               ).values(),
+            ] as T
+      );
+      // setData(result.results);
+      setLastPage(result.total_pages);
+
+      setLoading(false);
+   };
+
+   return { handleFetch, loading, error, data, lastPage };
+};
+
+export default useInfiniteFetch;
