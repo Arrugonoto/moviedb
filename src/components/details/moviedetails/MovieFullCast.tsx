5ef13863efdb4b661c1bb549665ba920dd3e757b
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-08-05 23:41:58 +0200

    rearrange(MovieFullCast): added component for displaying entire cast of movie

---
+++ src/components/details/moviedetails/MovieFullCast.tsx
@@ -0,0 +1,42 @@
+import { Container, Row, Text } from '@nextui-org/react';
+import CardCastThumbnail from '../../card/CardCastThumbnail';
+
+interface CastProps {
+   cast: {
+      adult: boolean;
+      gender: number;
+      id: number;
+      known_for_department: string;
+      name: string;
+      original_name: string;
+      popularity: number;
+      profile_path: string;
+      cast_id: number;
+      character: string;
+      credit_id: string;
+      order: number;
+   }[];
+}
+
+const MovieFullCast = ({ cast }: CastProps) => {
+   return (
+      <Container css={{ p: '1rem 0', fd: 'column' }}>
+         <Text h3 size={20}>
+            Cast
+         </Text>
+         <Row
+            css={{
+               display: 'grid',
+               gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+               gap: '1rem',
+            }}
+         >
+            {cast?.map(person => (
+               <CardCastThumbnail key={person?.id} person={person} />
+            ))}
+         </Row>
+      </Container>
+   );
+};
+
+export default MovieFullCast;
