b975f36e8e857e71178891769e91a6a7938c36ae
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-08-05 23:39:23 +0200

    update(MovieCredits): finished building page, improved styling, added data about movie actors and crew

--- src/pages/castandcrew/MovieCredits.tsx
+++ src/pages/castandcrew/MovieCredits.tsx
@@ -3,8 +3,10 @@ import { METHODS } from '../../services/api';
 import { API_KEY } from '../../services/api-key';
 import useFetch from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
-import { Container, Row, Col, Text, Image } from '@nextui-org/react';
+import { Container, Row, Text, Image } from '@nextui-org/react';
 import { BASE_URL, IMAGE_SIZE } from '../../data/imageConfig';
+import MovieFullCast from '../../components/details/moviedetails/MovieFullCast';
+import MovieFullCrew from '../../components/details/moviedetails/MovieFullCrew';
 
 interface OptionsTypes {
    method: string;
@@ -117,16 +119,16 @@ const MovieCredits = () => {
                <div
                   style={{
                      display: 'flex',
-                     justifyContent: 'center',
+                     justifyContent: 'space-between',
                      alignItems: 'center',
                      height: '100%',
                      overflow: 'hidden',
+                     width: '6rem',
                   }}
                >
                   <Image
                      src={`${BASE_URL}${IMAGE_SIZE.POSTER.W342}${data?.poster_path}`}
                      objectFit="cover"
-                     width="6rem"
                      alt="Movie poster"
                      css={{ borderRadius: '0.1rem' }}
                   />
@@ -158,71 +160,8 @@ const MovieCredits = () => {
                   </Row>
                </div>
             </Row>
-            <Row css={{ p: '1rem 0', fd: 'column' }}>
-               <Text h3 size={20}>
-                  Cast
-               </Text>
-               <Row
-                  css={{
-                     display: 'grid',
-                     gridTemplateColumns:
-                        'repeat(auto-fill, minmax(22rem, 1fr))',
-                     gap: '1rem',
-                  }}
-               >
-                  {data?.credits?.cast?.map(person => (
-                     <article
-                        key={person?.cast_id}
-                        style={{
-                           display: 'flex',
-                           width: '100%',
-                           height: '8rem',
-                           borderRadius: '0.4rem',
-                           overflow: 'hidden',
-                           gap: '1rem',
-                        }}
-                     >
-                        <div>
-                           {person?.profile_path ? (
-                              <Image
-                                 src={`${
-                                    BASE_URL +
-                                    IMAGE_SIZE.PROFILE.W185 +
-                                    person?.profile_path
-                                 }`}
-                                 width="6rem"
-                                 objectFit="cover"
-                              />
-                           ) : (
-                              <div
-                                 style={{
-                                    width: '6rem',
-                                    height: '100%',
-                                    background: 'rgb(105, 223, 27)',
-                                 }}
-                              ></div>
-                           )}
-                        </div>
-                        <Col css={{ d: 'flex', fd: 'column', jc: 'center' }}>
-                           <Row>
-                              <Text size={18} css={{ fontWeight: '600' }}>
-                                 {person?.name}
-                              </Text>
-                           </Row>
-                           <Row>
-                              <Text color="#9210a0">{person?.character}</Text>
-                           </Row>
-                        </Col>
-                     </article>
-                  ))}
-               </Row>
-            </Row>
-            <Row css={{ p: '1rem 0' }}>
-               <Text h3 size={20}>
-                  Crew
-               </Text>
-               <Row></Row>
-            </Row>
+            <MovieFullCast cast={data?.credits?.cast} />
+            <MovieFullCrew crew={data?.credits?.crew} />
          </Container>
       </section>
    );
