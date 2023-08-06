0ed94a9a7c81c3e8c05eae665dbaaece360d0f5f
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-08-06 14:50:16 +0200

    rearrange(MovieFullCrew): added component for displaying info about entire production crew

---
+++ src/components/details/moviedetails/MovieFullCrew.tsx
@@ -0,0 +1,287 @@
+import { Container, Text, Row } from '@nextui-org/react';
+import CardCastThumbnail from '../../card/CardCastThumbnail';
+
+interface CrewProps {
+   crew: {
+      adult: boolean;
+      gender: number;
+      id: number;
+      known_for_department: string;
+      name: string;
+      original_name: string;
+      popularity: number;
+      profile_path: string;
+      credit_id: string;
+      department: string;
+      job: string;
+   }[];
+}
+
+const MovieFullCrew = ({ crew }: CrewProps) => {
+   const directing = crew?.filter(person => person.department === 'Directing');
+   const writing = crew?.filter(person => person.department === 'Writing');
+   const production = crew?.filter(
+      person => person.department === 'Production'
+   );
+   const camera = crew?.filter(person => person.department === 'Camera');
+   const sound = crew?.filter(person => person.department === 'Sound');
+   const art = crew?.filter(person => person.department === 'Art');
+   const costume = crew?.filter(
+      person => person.department === 'Costume & Make-Up'
+   );
+   const effects = crew?.filter(
+      person => person.department === 'Visual Effects'
+   );
+   const editing = crew?.filter(person => person.department === 'Editing');
+   const staff = crew?.filter(person => person.department === 'Crew');
+
+   return (
+      <Container css={{ p: '1rem 0' }}>
+         <Text h3 size={20}>
+            Crew
+         </Text>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Directing
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {directing?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Writing
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {writing?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Production
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {production?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Camera
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {camera?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Sound
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {sound?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Art
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {art?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Costume & Make up
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {costume?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Visual Effects
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {effects?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Editing
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {editing?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+         <Row css={{ d: 'flex', fd: 'column', marginBottom: '2.5rem' }}>
+            <Text
+               h4
+               size={15}
+               css={{
+                  fontFamily: 'Roboto',
+                  letterSpacing: '0.09px',
+                  opacity: 0.8,
+               }}
+            >
+               Crew
+            </Text>
+            <Row
+               css={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(22rem, 1fr))',
+                  gap: '1rem',
+               }}
+            >
+               {staff?.map(person => (
+                  <CardCastThumbnail key={person?.credit_id} person={person} />
+               ))}
+            </Row>
+         </Row>
+      </Container>
+   );
+};
+
+export default MovieFullCrew;
