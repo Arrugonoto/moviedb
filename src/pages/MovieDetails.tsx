b53cc52d4bbf74e48934fcf6069047f84335341b
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-08-06 18:43:23 +0200

    update(MovieDetails): updated page

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import MovieDetailsSection from '../components/details/MovieDetailsSection';
 
 const MovieDetails = () => {
+   const { movieId } = useParams();
+
+   useEffect(() => {
+      window.scrollTo({
+         top: 0,
+      });
+   }, [movieId]);
+
    return (
-      <section style={{ minHeight: '100dvh' }}>
+      <section>
          <MovieDetailsSection />
       </section>
    );
