3ce59abb665782eecad65eecd72fcfbfd1c90dba
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-08-07 19:47:58 +0200

    update(routes): updated file with page routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,18 +1,18 @@
 const ROUTES: { [key: string]: string } = {
    HOME: 'moviedb',
    MOVIES: 'moviedb/movies',
-   MOVIE_GENRE: 'moviedb/movie/genre',
+   MOVIE_GENRE: 'moviedb/movie/genre', //moviedb/movie/genre/id
    MOVIES_POPULAR: 'moviedb/popular/movie',
    MOVIES_TOP_RATED: 'moviedb/top-rated/movie',
    MOVIES_UPCOMING: 'moviedb/upcoming/movies',
-   MOVIE_DETAILS: 'moviedb/movie/details', // /movie-title/id
+   MOVIE_DETAILS: 'moviedb/movie/details', //moviedb/movie/details/movie-name/id
    SERIES: 'moviedb/series',
    SERIES_GENRE: 'moviedb/series/genre',
    SERIES_POPULAR: 'moviedb/popular/series',
    SERIES_TOP_RATED: 'moviedb/top-rated/tv-series',
-   SERIES_DETAILS: 'moviedb/series/details', // /series-title/id
+   SERIES_DETAILS: 'moviedb/series/details', // moviedb/series/details/series-name/id
    PEOPLE: 'moviedb/person',
-   PERSON_DETAILS: 'moviedb/person/details', // /name-surname/id
+   PERSON_DETAILS: 'moviedb/person/details', //moviedb/person/details/name-surname/id
 };
 
 export default ROUTES;
