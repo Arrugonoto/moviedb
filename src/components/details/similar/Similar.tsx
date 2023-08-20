129301b84d99096e6c7c35586257ce646b40a736
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-08-20 23:06:31 +0200

    refactor(Recommendations, Similar): moved few elements from parent component

--- src/components/details/similar/Similar.tsx
+++ src/components/details/similar/Similar.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-import { Container, Text } from '@nextui-org/react';
+import { Container, Text, Row } from '@nextui-org/react';
 import { motion } from 'framer-motion';
 import { METHODS } from '../../../services/api';
 import { API_KEY } from '../../../services/api-key';
@@ -15,7 +15,7 @@ interface OptionsTypes {
    };
 }
 
-interface MovieProps {
+interface MovieTypes {
    adult: boolean;
    backdrop_path: string;
    genre_ids: number[];
@@ -35,7 +35,7 @@ interface MovieProps {
 const Similar = () => {
    const containerRef = useRef<HTMLDivElement>(null);
    const { movieId } = useParams();
-   const { handleFetch, data } = useFetch<MovieProps[]>([]);
+   const { handleFetch, data } = useFetch<MovieTypes[]>([]);
 
    const fetchData = async (): Promise<void> => {
       const options: OptionsTypes = {
@@ -56,63 +56,71 @@ const Similar = () => {
    }, [movieId]);
 
    return (
-      <Container css={{ p: '0' }}>
-         {data?.length > 0 ? (
-            <motion.div
-               ref={containerRef}
-               style={{
-                  display: 'flex',
-                  overflowX: 'hidden',
-                  gap: '1.2rem',
-                  borderRadius: '.2rem',
-                  padding: '0 1rem',
-               }}
+      <Container css={{ d: 'flex', fd: 'column', p: '1rem 0' }}>
+         <Row>
+            <Text
+               h3
+               css={{ borderLeft: '5px solid #9210a0', paddingLeft: '0.4rem' }}
             >
+               Similar
+            </Text>
+         </Row>
+         <Row>
+            {data?.length > 0 ? (
                <motion.div
-                  drag="x"
-                  dragConstraints={containerRef}
+                  ref={containerRef}
                   style={{
                      display: 'flex',
-                     gap: '1.2rem',
-                     padding: '2rem 1.4rem',
+                     overflowX: 'hidden',
+                     borderRadius: '.2rem',
                   }}
-                  dragElastic={0.1}
                >
-                  {data?.map((movie, i) => {
-                     if (i < 10)
-                        return (
-                           <CardMdBlur
-                              key={movie.id}
-                              id={movie.id}
-                              overview={movie.overview}
-                              popularity={movie.popularity}
-                              poster_path={movie.poster_path}
-                              release_date={movie.release_date}
-                              title={movie.title}
-                              vote_average={movie.vote_average}
-                              vote_count={movie.vote_count}
-                           />
-                        );
-                  })}
+                  <motion.div
+                     drag="x"
+                     dragConstraints={containerRef}
+                     style={{
+                        display: 'flex',
+                        gap: '1.2rem',
+                        padding: '2rem 1.4rem',
+                     }}
+                     dragElastic={0.1}
+                  >
+                     {data?.map((movie, i) => {
+                        if (i < 10)
+                           return (
+                              <CardMdBlur
+                                 key={movie.id}
+                                 id={movie.id}
+                                 overview={movie.overview}
+                                 popularity={movie.popularity}
+                                 poster_path={movie.poster_path}
+                                 release_date={movie.release_date}
+                                 title={movie.title}
+                                 vote_average={movie.vote_average}
+                                 vote_count={movie.vote_count}
+                              />
+                           );
+                     })}
+                  </motion.div>
                </motion.div>
-            </motion.div>
-         ) : (
-            <div
-               style={{
-                  background:
-                     'linear-gradient(175deg, #9210a04c, transparent 60%)',
-                  width: '80%',
-                  padding: '1rem 2rem',
-                  borderRadius: '0.5rem',
-                  boxShadow: '0 0 0.8rem 0 rgba(0, 0, 0, 0.2)',
-               }}
-            >
-               <Text>
-                  There aren't any recommended similar positions for currently
-                  watched Title.
-               </Text>
-            </div>
-         )}
+            ) : (
+               <div
+                  style={{
+                     background:
+                        'linear-gradient(175deg, #9210a04c, transparent 60%)',
+                     width: '80%',
+                     padding: '1rem 2rem',
+                     borderRadius: '0.5rem',
+                     boxShadow: '0 0 0.8rem 0 rgba(0, 0, 0, 0.2)',
+                  }}
+               >
+                  <Text>
+                     There aren't any recommended similar positions for
+                     currently watched Title.
+                  </Text>
+               </div>
+            )}
+         </Row>
       </Container>
    );
 };
