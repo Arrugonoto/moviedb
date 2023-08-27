43d02a28e6a9dc9d11c49535f8e8a2c98eb75b3a
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-08-27 22:08:00 +0200

    update(PersonDetailsSection): added types for fetched data about person, updated component structure, updated styling

--- src/components/details/PersonDetailsSection.tsx
+++ src/components/details/PersonDetailsSection.tsx
@@ -1,5 +1,76 @@
+import { useEffect } from 'react';
+import { Container, Row } from '@nextui-org/react';
+import useFetch from '../../hooks/useFetch';
+import { METHODS } from '../../services/api';
+import { API_KEY } from '../../services/api-key';
+import DetailsHeader from './persondetails/DetailsHeader';
+import { useParams } from 'react-router-dom';
+import KnownFrom from './persondetails/KnownFrom';
+import Filmography from './persondetails/Filmography';
+
+interface OptionsTypes {
+   method: string;
+   headers: {
+      accept: string;
+      Authorization: string;
+   };
+}
+
+interface PersonDetailsTypes {
+   adult: false;
+   also_known_as: string[];
+   biography: string;
+   birthday: string;
+   deathday: string;
+   gender: number;
+   homepage: string;
+   id: number;
+   imdb_id: string;
+   known_for_department: string;
+   name: string;
+   place_of_birth: string;
+   popularity: number;
+   profile_path: string;
+}
+
 const PersonDetailsSection = () => {
-   return <div>PersonDetailsSection</div>;
+   const { personId } = useParams();
+   const { handleFetch, data } = useFetch<PersonDetailsTypes>(
+      {} as PersonDetailsTypes
+   );
+
+   const fetchData = async (): Promise<void> => {
+      const options: OptionsTypes = {
+         method: METHODS.GET,
+         headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY.access_token}`,
+         },
+      };
+
+      const url = `https://api.themoviedb.org/3/person/${personId}?language=en-US`;
+
+      handleFetch({ url, options });
+   };
+
+   useEffect(() => {
+      fetchData();
+      // eslint-disable-next-line
+   }, [personId]);
+
+   return (
+      <Container fluid css={{ d: 'flex', p: '0', fd: 'column', gap: '3rem' }}>
+         <Row>
+            <DetailsHeader person={data} />
+         </Row>
+         <Row>
+            <KnownFrom />
+         </Row>
+         <Row>
+            <Filmography />
+         </Row>
+      </Container>
+   );
 };
 
 export default PersonDetailsSection;
