96febd99e0ed44a0b201226ef538061e3dd0f891
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-08-30 21:24:14 +0200

    update(Cast): added info about movies/tv shows related to watched person,updated styling

--- src/components/details/persondetails/Cast.tsx
+++ src/components/details/persondetails/Cast.tsx
@@ -1,5 +1,6 @@
 import { Container, Row, Text, Image } from '@nextui-org/react';
 import { BASE_URL, IMAGE_SIZE } from '../../../data/imageConfig';
+import { BiSolidStar } from 'react-icons/bi';
 
 interface PropTypes {
    cast: {
@@ -43,35 +44,118 @@ const Cast = ({ cast }: PropTypes) => {
 
    return (
       <Container>
-         <Row>
-            <Text>Acting</Text>
+         <Row css={{ marginBottom: '2rem' }}>
+            <Text h3 css={{ fontSize: '1.3rem' }}>
+               Acting
+            </Text>
          </Row>
-         <Row css={{ d: 'flex', fd: 'column' }}>
+         <Row css={{ d: 'flex', fd: 'column', gap: '1rem' }}>
             {sortedProductions?.map(production => (
                <article
                   key={production?.credit_id}
-                  style={{ display: 'flex', gap: '1rem' }}
+                  style={{
+                     display: 'flex',
+                     width: '80%',
+                     gap: '1rem',
+                     minHeight: '7rem',
+                     paddingBottom: '1rem',
+                     borderBottom: '1px solid rgba(180,180,180, 0.5)',
+                  }}
                >
-                  <div style={{ width: '6rem' }}>
-                     <Image
-                        src={`${BASE_URL}${IMAGE_SIZE.POSTER.W342}/${production.poster_path}`}
-                        objectFit="cover"
-                        width="100%"
-                        loading="lazy"
-                        alt="Production Poster"
-                     />
+                  <div
+                     style={{
+                        minWidth: '5rem',
+                        width: '5rem',
+                        borderRadius: '0.1rem',
+                        overflow: 'hidden',
+                     }}
+                  >
+                     {production?.poster_path ? (
+                        <Image
+                           src={`${BASE_URL}${IMAGE_SIZE.POSTER.W342}/${production.poster_path}`}
+                           objectFit="cover"
+                           width="100%"
+                           loading="lazy"
+                           alt="Production Poster"
+                        />
+                     ) : (
+                        <div
+                           style={{
+                              width: '100%',
+                              height: '100%',
+                              backgroundColor: 'rgba(180,180,180, 0.3)',
+                           }}
+                        ></div>
+                     )}
                   </div>
-                  <div>
-                     <Text>
-                        {production?.title
-                           ? production?.title
-                           : production?.name}
-                     </Text>
-                     <Text>
-                        {production?.release_date
-                           ? production?.release_date?.slice(0, 4)
-                           : production?.first_air_date?.slice(0, 4)}
-                     </Text>
+                  <div
+                     style={{
+                        display: 'flex',
+                        width: '100%',
+                        justifyContent: 'space-between',
+                     }}
+                  >
+                     <div
+                        style={{
+                           display: 'flex',
+                           flexDirection: 'column',
+                           justifyContent: 'space-between',
+                        }}
+                     >
+                        <Row css={{ fd: 'column' }}>
+                           <Row>
+                              <Text
+                                 h4
+                                 css={{
+                                    fontFamily: 'Roboto',
+                                    fontSize: '1rem',
+                                    letterSpacing: '0.1px',
+                                 }}
+                              >
+                                 {production?.title
+                                    ? production?.title
+                                    : production?.name}
+                              </Text>
+                           </Row>
+                           <Row
+                              style={{ display: 'flex', alignItems: 'center' }}
+                           >
+                              <BiSolidStar
+                                 style={{
+                                    fontSize: '1.1rem',
+                                    color: '#9210A0',
+                                 }}
+                              />
+                              <Text css={{ fontSize: '1.1rem' }}>
+                                 {production?.vote_average?.toFixed(1)}
+                              </Text>
+                           </Row>
+                        </Row>
+                        <Row css={{ gap: '0.2rem' }}>
+                           <Text>{`${production?.character}`}</Text>
+                           {production?.episode_count && (
+                              <Text css={{ opacity: 0.8 }}>
+                                 {`(${production?.episode_count} ${
+                                    production?.episode_count > 1
+                                       ? 'episodes'
+                                       : 'episode'
+                                 })`}
+                              </Text>
+                           )}
+                        </Row>
+                     </div>
+                     <div
+                        style={{
+                           display: 'flex',
+                           alignItems: 'center',
+                        }}
+                     >
+                        <Text css={{ fontSize: '1.2rem' }}>
+                           {production?.release_date
+                              ? production?.release_date?.slice(0, 4)
+                              : production?.first_air_date?.slice(0, 4)}
+                        </Text>
+                     </div>
                   </div>
                </article>
             ))}
