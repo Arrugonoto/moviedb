5444a014a2bbf656c5c47a9caa991b966399d1fd
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-09-03 19:16:39 +0200

    update(Home): added scroll to top of page, on page render

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Container } from '@nextui-org/react';
 import Trending from '../components/trending/Trending';
 import Popular from '../components/popular/Popular';
@@ -6,6 +7,10 @@ import Upcoming from '../components/upcoming/Upcoming';
 import NowPlaying from '../components/nowplaying/NowPlaying';
 
 const Home = () => {
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, []);
+
    return (
       <Container
          fluid
