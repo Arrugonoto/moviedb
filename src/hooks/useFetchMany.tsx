3a34d0fdfdd3fadbb4d9fb5f7f4ab81799f815c5
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-09-08 21:27:26 +0200

    rearrange/feat(useFetchMany): add hook for handling fetch of multiple sources with Pomise.all method

---
+++ src/hooks/useFetchMany.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+interface FetchTypes {
+   urls: string[];
+   options?: Parameters<typeof fetch>[1];
+}
+
+const useFetchMany = <T,>(initialData: T) => {
+   const [loading, setLoading] = useState<boolean>(false);
+   const [error, setError] = useState<unknown>(null);
+   const [data, setData] = useState<T[]>([initialData]);
+
+   const handleFetchMany = async ({
+      urls,
+      options,
+   }: FetchTypes): Promise<void> => {
+      try {
+         setLoading(true);
+
+         const promises: Promise<T>[] = urls.map(async url => {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+               throw new Error(`Couldn't fetch source`);
+            }
+            return response.json() as Promise<T>;
+         });
+
+         const results: T[] = await Promise.all(promises);
+
+         setData(results);
+      } catch (e: unknown) {
+         console.error('Error:', e);
+         if (e instanceof Error) {
+            setError(e);
+         } else {
+            setError(new Error('An unknown error occurred.'));
+         }
+      }
+      setLoading(false);
+   };
+
+   return { handleFetchMany, loading, error, data };
+};
+
+export default useFetchMany;
