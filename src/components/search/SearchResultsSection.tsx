4c9f8a03444bbf75705a2d03b7b1552cbbbd67ef
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-09-10 21:10:53 +0200

    update(SearchResultsSection): updated styling, updated types, passed and configured fetched data

--- src/components/search/SearchResultsSection.tsx
+++ src/components/search/SearchResultsSection.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Container, Row, Button } from '@nextui-org/react';
+import { Container, Row, Button, Text } from '@nextui-org/react';
 import ResultsSummary from './results/ResultsSummary';
 import ResultsPeople from './results/ResultsPeople';
 import ResultsMovies from './results/ResultsMovies';
@@ -8,6 +8,7 @@ import { useSearchParams } from 'react-router-dom';
 import { METHODS } from '../../services/api';
 import { API_KEY } from '../../services/api-key';
 import useFetch from '../../hooks/useFetch';
+import useFetchMany from '../../hooks/useFetchMany';
 
 interface OptionsTypes {
    method: string;
@@ -34,6 +35,8 @@ interface SearchTypes {
       title: string;
       original_language: string;
       original_title: string;
+      name: string;
+      original_name: string;
       overview: string;
       poster_path: string;
       media_type: string;
@@ -59,12 +62,24 @@ interface SearchTypes {
    origin_country: string[];
 }
 
+interface SearchManyTypes {
+   page: number;
+   results: SearchTypes[];
+   total_pages: number;
+   total_results: number;
+}
+
 const SearchResultsSection = () => {
    const [activeTab, setActiveTab] = useState<string>('Summary');
    const [page, setPage] = useState<number>(1);
    const [searchParams] = useSearchParams();
    const queryString = searchParams.get('q');
-   const { handleFetch, data, lastPage } = useFetch<SearchTypes[]>([]);
+   const { handleFetch, data, lastPage, numberOfResults } = useFetch<
+      SearchTypes[]
+   >([]);
+   const { handleFetchMany, data: dataMany } = useFetchMany<SearchManyTypes>(
+      {} as SearchManyTypes
+   );
 
    const fetchData = async (): Promise<void> => {
       const options: OptionsTypes = {
@@ -80,8 +95,27 @@ const SearchResultsSection = () => {
       handleFetch({ url, options });
    };
 
+   const fetchManyData = async (): Promise<void> => {
+      const options: OptionsTypes = {
+         method: METHODS.GET,
+         headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY.access_token}`,
+         },
+      };
+
+      const urls = [
+         `https://api.themoviedb.org/3/search/person?query=${queryString}&include_adult=false&language=en-US&page=1`,
+         `https://api.themoviedb.org/3/search/movie?query=${queryString}&include_adult=false&language=en-US&page=1`,
+         `https://api.themoviedb.org/3/search/tv?query=${queryString}&include_adult=false&language=en-US&page=1`,
+      ];
+
+      handleFetchMany({ urls, options });
+   };
+
    useEffect(() => {
       fetchData();
+      fetchManyData();
       setActiveTab('Summary');
       // eslint-disable-next-line
    }, [queryString]);
@@ -90,18 +124,69 @@ const SearchResultsSection = () => {
       <Container css={{ d: 'flex', fd: 'column', gap: '2rem' }}>
          <Row justify="center">
             <Button.Group light rounded size="lg">
-               <Button onPress={() => setActiveTab('Summary')}>Total</Button>
-               <Button onPress={() => setActiveTab('People')}>People</Button>
-               <Button onPress={() => setActiveTab('Movies')}>Movies</Button>
+               <Button onPress={() => setActiveTab('Summary')}>
+                  <Text
+                     h2
+                     css={{
+                        color: activeTab === 'Summary' ? '#9210a0' : '',
+                        fontSize: '1.2rem',
+                     }}
+                  >
+                     Total
+                  </Text>
+               </Button>
+               <Button onPress={() => setActiveTab('People')}>
+                  <Text
+                     h2
+                     css={{
+                        color: activeTab === 'People' ? '#9210a0' : '',
+                        fontSize: '1.2rem',
+                     }}
+                  >
+                     People
+                  </Text>
+                  <span style={{ marginLeft: '0.3rem', opacity: 0.5 }}>
+                     ({dataMany?.[0]?.total_results})
+                  </span>
+               </Button>
+               <Button onPress={() => setActiveTab('Movies')}>
+                  <Text
+                     h2
+                     css={{
+                        color: activeTab === 'Movies' ? '#9210a0' : '',
+                        fontSize: '1.2rem',
+                     }}
+                  >
+                     Movies
+                  </Text>
+                  <span style={{ marginLeft: '0.3rem', opacity: 0.5 }}>
+                     ({dataMany?.[1]?.total_results})
+                  </span>
+               </Button>
                <Button onPress={() => setActiveTab('TvShows')}>
-                  TV Series
+                  <Text
+                     h2
+                     css={{
+                        color: activeTab === 'TvShows' ? '#9210a0' : '',
+                        fontSize: '1.2rem',
+                     }}
+                  >
+                     TV Series
+                  </Text>
+                  <span style={{ marginLeft: '0.3rem', opacity: 0.5 }}>
+                     ({dataMany?.[2]?.total_results})
+                  </span>
                </Button>
             </Button.Group>
          </Row>
          <Row css={{ fd: 'column', gap: '1rem' }}>
-            <Row>Results: {data?.length}</Row>
             <Row>
-               {activeTab === 'Summary' && <ResultsSummary />}
+               <Text h3 css={{ fontSize: '1.2rem' }}>
+                  Results: {numberOfResults}
+               </Text>
+            </Row>
+            <Row>
+               {activeTab === 'Summary' && <ResultsSummary results={data} />}
                {activeTab === 'People' && <ResultsPeople />}
                {activeTab === 'Movies' && <ResultsMovies />}
                {activeTab === 'TvShows' && <ResultsTvShows />}
