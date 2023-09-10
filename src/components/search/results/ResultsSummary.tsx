3b81284ad1692d098c7f137625711067e76b5e3e
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-09-10 21:12:51 +0200

    update(ResultsSummary): updated styling, types, imported component with info about fetched result

--- src/components/search/results/ResultsSummary.tsx
+++ src/components/search/results/ResultsSummary.tsx
@@ -1,5 +1,60 @@
-const ResultsSummary = () => {
-   return <div>ResultsSummary</div>;
+import { Container } from '@nextui-org/react';
+import CardSearchResult from '../../card/CardSearchResult';
+
+interface PropTypes {
+   results: {
+      adult: boolean;
+      id: number;
+      name?: string;
+      original_name?: string;
+      media_type: string;
+      popularity: number;
+      gender?: number;
+      known_for_department: string;
+      profile_path: string;
+      known_for?: {
+         adult: boolean;
+         backdrop_path: string;
+         id: number;
+         title: string;
+         original_language: string;
+         original_title: string;
+         name: string;
+         original_name: string;
+         overview: string;
+         poster_path: string;
+         media_type: string;
+         genre_ids: number[];
+         popularity: number;
+         release_date: string;
+         video: boolean;
+         vote_average: number;
+         vote_count: number;
+      }[];
+      backdrop_path: string;
+      title?: string;
+      original_title?: string;
+      original_language: string;
+      overview: string;
+      poster_path: string;
+      genre_ids?: number[];
+      release_date?: string;
+      video: false;
+      vote_average: number;
+      vote_count: number;
+      first_air_date?: string;
+      origin_country?: string[];
+   }[];
+}
+
+const ResultsSummary = ({ results }: PropTypes) => {
+   return (
+      <Container fluid css={{ d: 'flex', fd: 'column', gap: '1rem' }}>
+         {results?.map(result => (
+            <CardSearchResult key={result?.id} result={result} />
+         ))}
+      </Container>
+   );
 };
 
 export default ResultsSummary;
