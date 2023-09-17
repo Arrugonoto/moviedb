9a6cf23dde799e52143363492676453ff45c908e
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-09-17 20:03:57 +0200

    update(subMenu): added constant for mobile menu links, updated links for dropdown submenu

--- src/data/subMenu.ts
+++ src/data/subMenu.ts
@@ -1,12 +1,30 @@
 import ROUTES from '../routes/routes';
 
-export const MOVIES: { name: string; route: string }[] = [
-   { name: 'Popular', route: ROUTES.MOVIES_POPULAR },
-   { name: 'Upcoming', route: ROUTES.MOVIES_UPCOMING },
-   { name: 'Top Rated', route: ROUTES.MOVIES_TOP_RATED },
-];
+export const SUBMENU: { [key: string]: { name: string; route: string }[] } = {
+   MOVIES: [
+      { name: 'Popular', route: ROUTES.MOVIES_POPULAR },
+      { name: 'Upcoming', route: ROUTES.MOVIES_UPCOMING },
+      { name: 'Top Rated', route: ROUTES.MOVIES_TOP_RATED },
+   ],
+   SERIES: [
+      { name: 'Popular', route: ROUTES.SERIES_POPULAR },
+      { name: 'Top Rated', route: ROUTES.SERIES_TOP_RATED },
+   ],
+   TMDB: [
+      { name: 'Home', route: 'https://www.themoviedb.org/' },
+      { name: 'TMDB API', route: 'https://developer.themoviedb.org/docs' },
+   ],
+};
 
-export const SERIES: { name: string; route: string }[] = [
-   { name: 'Popular', route: ROUTES.SERIES_POPULAR },
-   { name: 'Top Rated', route: ROUTES.SERIES_TOP_RATED },
+export const MENU_ITEMS: { name: string; route: string }[] = [
+   { name: 'Movies', route: '' },
+   { name: 'Popular Movies', route: ROUTES.MOVIES_POPULAR },
+   { name: 'Upcoming Movies', route: ROUTES.MOVIES_UPCOMING },
+   { name: 'Top Rated Movies', route: ROUTES.MOVIES_TOP_RATED },
+   { name: 'TV Shows', route: '' },
+   { name: 'Popular Shows', route: ROUTES.SERIES_POPULAR },
+   { name: 'Top Rated Shows', route: ROUTES.SERIES_TOP_RATED },
+   { name: 'People', route: '' },
+   { name: 'The Movie Database', route: 'https://www.themoviedb.org/' },
+   { name: 'TMDB API', route: 'https://developer.themoviedb.org/docs' },
 ];
