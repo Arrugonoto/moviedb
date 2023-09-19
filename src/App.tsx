acecfa25af273d83e407410aafaeb689fa25bbcd
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-09-19 20:08:18 +0200

    refactor(App): removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 // react-router-dom
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // nextui
 
 import {
